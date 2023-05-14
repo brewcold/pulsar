892d1a13b9055a9093c5aa0b7361a83eb9f43fc5
bmincof <bminlvcoffee@gmail.com>
2023-05-14 19:50:11 +0900

    feat: 서버 초기 설정

---
+++ server/pulsar/src/main/java/com/hammer/pulsar/PulsarApplication.java
@@ -0,0 +1,13 @@
+package com.hammer.pulsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsarApplication.class, args);
+	}
+
+}
