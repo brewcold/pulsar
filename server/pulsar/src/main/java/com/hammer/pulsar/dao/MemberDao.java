2b6c34de226499c19400c5e99bf52c7fdf77f7d3
bmincof <bminlvcoffee@gmail.com>
2023-05-16 23:17:21 +0900

    fix: MemberDao에 누락된 메서드 추가

--- server/pulsar/src/main/java/com/hammer/pulsar/dao/MemberDao.java
+++ server/pulsar/src/main/java/com/hammer/pulsar/dao/MemberDao.java
@@ -1,5 +1,6 @@
 package com.hammer.pulsar.dao;
 
+import com.hammer.pulsar.dto.member.MemberProfile;
 import com.hammer.pulsar.dto.member.MemberRegistRequest;
 
 // Member 테이블과 통신하기 위한 DAO
@@ -12,6 +13,10 @@ public interface MemberDao {
 
     // 닉네임 중복 검사를 위한 메서드
     public boolean findNickname(String nickname);
+
+    // 프로필 조회를 위한 메서드
+    public MemberProfile selectProfileByMemberId(int memberId);
+
 
     // 회원 정보 수정을 위한 메서드
     public int updateMember(MemberRegistRequest request);
