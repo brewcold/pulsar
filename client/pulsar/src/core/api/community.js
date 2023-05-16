5eeee2a7c4e5029b6b238ea592c9947f42e28289
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-16 11:57:18 +0900

    feat: 디렉토리 구조 및 라우팅 설정

---
+++ client/pulsar/src/core/api/community.js
@@ -0,0 +1,17 @@
+//게시판 글 리스트 보기
+
+//글 상세보기 - static datas
+
+//글 상세보기 - active data - 북마크 여부 확인
+
+//글 상세보기 - active data - 북마크하기, 북마크 취소하기
+
+//글 상세보기 - active data - 추천 수 가져오기
+
+//글 상세보기 - active data - 추천하기, 추천 취소하기
+
+//글 상세보기 - active data - 댓글 가져오기
+
+//글 상세보기 - active data - 댓글 작성
+
+//글 상세보기 - active data - 댓글 삭제
---
+++ client/pulsar/src/module/template.vue
@@ -0,0 +1,22 @@
+<!--라우팅과 -->
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+/* 
+Global CSS Import
+*/
+</style>
+
+<script>
+export default {
+  name: 'template',
+  components: {},
+  created() {
+    //fetch
+  },
+};
+</script>
