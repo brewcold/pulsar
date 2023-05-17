e939485d3481caecc0d3fb4b6eb3015ea326cff8
bmincof <bminlvcoffee@gmail.com>
2023-05-17 19:48:33 +0900

    feat: 도메인별 컨트롤러 초기 구현 완료

--- server/pulsar/src/main/java/com/hammer/pulsar/controller/CommunityController.java
+++ server/pulsar/src/main/java/com/hammer/pulsar/controller/CommunityController.java
@@ -1,11 +1,15 @@
 package com.hammer.pulsar.controller;
 
+import com.hammer.pulsar.dto.NotDetermined;
+import com.hammer.pulsar.dto.article.Article;
+import com.hammer.pulsar.dto.article.ArticlePreview;
 import com.hammer.pulsar.service.ArticleService;
 import com.hammer.pulsar.service.InteractionService;
-import com.hammer.pulsar.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // 커뮤니티 관련 API 요청을 처리할 REST 컨트롤러
 @RestController
@@ -22,4 +26,43 @@ public class CommunityController {
         this.articleService = articleService;
         this.interactionService = interactionService;
     }
+
+    // 게시글 목록 조회 API
+    @GetMapping()
+    public ResponseEntity<List<ArticlePreview>> showAllArticles() {
+        return null;
+    }
+
+    // 글 상세보기 API
+    @GetMapping("/{articleId}/static")
+    public ResponseEntity<Article> showArticleDetail(@PathVariable int articleId) {
+        return null;
+    }
+
+    // 글 작성하기 API
+    @PostMapping()
+    public ResponseEntity<Integer> writeArticle(NotDetermined request) {
+        return null;
+    }
+
+    // 글 수정하기 API
+    @PutMapping()
+    public ResponseEntity<Void> modifyArticle(NotDetermined request) {
+        return null;
+    }
+
+    // 글 삭제하기 API
+    @DeleteMapping("/{articleId}")
+    public ResponseEntity<Void> removeArticle(@PathVariable int articleId) {
+        return null;
+    }
+
+    // GET 요청이 더 좋아보이기도 합니다.
+    // 응답에 검색조건에 맞는 게시글 목록이 들어가야 할 것 같습니다.
+    // 검색 API
+    @PostMapping("/search")
+    public ResponseEntity<List<ArticlePreview>> searchArticles(NotDetermined searchCondition) {
+        return null;
+    }
+
 }
--- server/pulsar/src/main/java/com/hammer/pulsar/controller/RoutineController.java
+++ server/pulsar/src/main/java/com/hammer/pulsar/controller/RoutineController.java
@@ -1,10 +1,13 @@
 package com.hammer.pulsar.controller;
 
-import com.hammer.pulsar.service.InteractionService;
+import com.hammer.pulsar.dto.NotDetermined;
+import com.hammer.pulsar.dto.routine.Routine;
 import com.hammer.pulsar.service.RoutineService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // 루틴 관련 API 요청을 처리할 REST 컨트롤러
 @RestController
@@ -19,4 +22,55 @@ public class RoutineController {
     public RoutineController(RoutineService routineService) {
         this.routineService = routineService;
     }
+
+    // 루틴 리스트 요청 API
+    @GetMapping("/{memberId}")
+    public ResponseEntity<List<Routine>> showAllRoutines(@PathVariable int memberId) {
+        return null;
+    }
+
+    // 루틴 상세보기 요청 API
+    @GetMapping("/routine/{routineId}")
+    public ResponseEntity<Routine> showRoutine(@PathVariable int routineId) {
+        return null;
+    }
+
+    // 루틴 작성 API
+    @PostMapping("/routine")
+    public ResponseEntity<Integer> addNewRoutine(NotDetermined request) {
+        return null;
+    }
+
+    // routineNo을 REQ에 포함하고 URI를 /routine 으로 변경하는 것이 깔끔해 보입니다.
+    // 루틴 수정 API
+    @PutMapping("/routine/{routineId}")
+    public ResponseEntity<Void> modifyRoutineInfo(@PathVariable int routineId, NotDetermined request) {
+        return null;
+    }
+
+    // 루틴 삭제 API
+    @DeleteMapping("/routine/{routineId}")
+    public ResponseEntity<Void> removeRoutine(@PathVariable int routineId) {
+        return null;
+    }
+
+    // URI를 routine/plan 으로 변경하는 것도 깔끔할 것 같습니다.
+    // 운동 계획 작성 API
+    @PostMapping("/routine/{routineId}")
+    public ResponseEntity<Void> addNewPlan(@PathVariable int routineId) {
+        return null;
+    }
+
+    // 운동 계획 수정 API
+    @PutMapping("/routine/{routineId}/{exerciseId}")
+    public ResponseEntity<Void> modifyPlan(@PathVariable int routineId, @PathVariable int exerciseId) {
+        return null;
+    }
+
+    // 운동 계획 삭제 API
+    @DeleteMapping("/routine/{routineId}/{exerciseId}")
+    public ResponseEntity<Void> removePlan(@PathVariable int routineId, @PathVariable int exerciseId) {
+        return null;
+    }
+
 }
