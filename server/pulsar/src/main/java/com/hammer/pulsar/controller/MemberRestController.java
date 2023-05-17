8b190a9a87c9a74b08879b0896a5e2c86932fa5a
bmincof <bminlvcoffee@gmail.com>
2023-05-17 17:55:35 +0900

    feat: 회원 관련 API 요청을 처리할 컨트롤러 초기 구현 완료

---
+++ server/pulsar/src/main/java/com/hammer/pulsar/controller/MemberRestController.java
@@ -0,0 +1,102 @@
+package com.hammer.pulsar.controller;
+
+import com.hammer.pulsar.dto.NotDetermined;
+import com.hammer.pulsar.dto.article.ArticlePreview;
+import com.hammer.pulsar.dto.member.MemberRegistRequest;
+import com.hammer.pulsar.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// 회원 관련 API 요청을 처리할 REST 컨트롤러
+@RestController
+@RequestMapping("/member")
+public class MemberRestController {
+
+    // 회원 관련 서비스 로직을 처리할 클래스
+    private final MemberService memberService;
+
+    // 컨트롤러의 생성자
+    @Autowired
+    public MemberRestController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // 회원가입 요청 API
+    @PostMapping("/signup")
+    public ResponseEntity<Void> registMember(MemberRegistRequest request) {
+        return null;
+    }
+
+    // 이메일 중복 검사 요청 API
+    @PostMapping("/signup/validation/email")
+    public ResponseEntity<Void> checkValidEmail(String email) {
+        return null;
+    }
+
+    // 닉네임 중복 검사 요청 API
+    @PostMapping("/signup/validation/nickname")
+    public ResponseEntity<Void> checkValidNickname(String nickname) {
+        return null;
+    }
+
+    // 로그인 API
+    // 상세 로직을 정하지 못했음
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(String email, String password) {
+        return null;
+    }
+
+    // 로그아웃 API
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        return null;
+    }
+
+    // 회원정보 수정 API
+    // 먼저 화면에 보여줄 현재 회원 정보
+    @GetMapping("/{memberId}")
+    public ResponseEntity<NotDetermined> showMemberInfo(@PathVariable int memberId) {
+        return null;
+    }
+
+    // 회원정보 수정 API
+    // 작성한 정보로 수정 요청
+    @PostMapping("/{memberId}")
+    public ResponseEntity<Void> modifyMemberInfo(@PathVariable int memberId) {
+        return null;
+    }
+
+    // 회원 탈퇴 API
+    @PostMapping("/{memberId}/quit")
+    public ResponseEntity<Void> quitMember(@PathVariable int memberId) {
+        return null;
+    }
+
+    // 내 활동내역 보기 - 북마크한 글 API
+    @GetMapping("/{memberId}/history/bookmark")
+    public ResponseEntity<List<ArticlePreview>> showBookmarked(@PathVariable int memberId) {
+        return null;
+    }
+
+    // 내 활동내역 보기 - 작성한 글 API
+    @GetMapping("/{memberId}/history/myarticle")
+    public ResponseEntity<List<ArticlePreview>> showWritten(@PathVariable int memberId) {
+        return null;
+    }
+
+    // 내 활동내역 보기 - 추천한 글 API
+    @GetMapping("/{memberId}/history/recommended")
+    public ResponseEntity<List<ArticlePreview>> showLiked(@PathVariable int memberId) {
+        return null;
+    }
+
+    // 내 활동내역 보기 - 댓글 작성한 글 API
+    @GetMapping("/{memberId}/history/comment")
+    public ResponseEntity<List<NotDetermined>> showCommented(@PathVariable int memberId) {
+        return null;
+    }
+
+}
