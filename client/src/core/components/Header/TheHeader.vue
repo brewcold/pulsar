60efc87d46e5c05de3a814423d9405cf61b52894
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-18 15:25:49 +0900

    feat: 전체 레이아웃 구조 작성

---
+++ client/src/core/components/Header/TheHeader.vue
@@ -0,0 +1,37 @@
+<!--특정 페이지에는 헤더와 내비게이션이 없어야 합니다.-->
+<!--레이아웃 페이지는 라우트와 함께 전체 사이트 골격을 잡습니다.-->
+<template>
+  <div id="header_container">
+    <div id="profile" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
+
+<style scoped>
+@import url('@/assets/css/init.css');
+@import url('@/assets/css/root.css');
+@import url('@/assets/css/typography.css');
+
+#header_container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: sticky;
+  border-bottom: 1px solid var(--light-color-lightgrey);
+  background: var(--light-color-white);
+  width: 100%;
+  height: 3.5rem;
+}
+#profile {
+  margin-left: 1rem;
+  border: 1px solid var(--light-color-darkgrey);
+  border-radius: 2rem;
+  width: 2rem;
+  height: 2rem;
+}
+</style>
---
+++ client/src/core/components/layout/AboutBtn.vue
@@ -0,0 +1,61 @@
+<!--버튼입니다. 누르면 상태가 토글되고, about 페이지가 바뀝니다.-->
+<!--Container이 flex로서 안의 버튼을 정가운데에 위치시킵니다. (hover 때문에 사이즈가 바뀌어도)-->
+<template>
+  <div id="aboutBtn_container">
+    <button id="aboutBtn">?</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutBtn',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+@import url('@/assets/css/init.css');
+@import url('@/assets/css/root.css');
+@import url('@/assets/css/typography.css');
+
+#aboutBtn_container {
+  width: 3rem;
+  height: 3rem;
+  position: fixed;
+  right: 1rem;
+  top: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#aboutBtn {
+  border-radius: 24px;
+  border: 1px solid var(--light-color-darkgrey);
+  color: var(--light-color-darkgrey);
+  background: white;
+  cursor: pointer;
+  height: 3rem;
+  width: 3rem;
+  font-size: 1.25rem;
+  z-index: 99;
+}
+
+#aboutBtn:hover {
+  width: 2.75rem;
+  height: 2.75rem;
+  font-size: 1rem;
+}
+
+#aboutBtn:active {
+  background: var(--light-color-lightgrey);
+}
+
+@media screen and (max-width: 600px) {
+  #about {
+    display: none;
+  }
+}
+</style>
