30fc872c821673a420b61f191b41c38b97a0c4b5
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-18 17:43:15 +0900

    feat: Layout 파일 분리

---
+++ client/src/core/components/Layout/EmptyLayout.vue
@@ -0,0 +1,36 @@
+<!--레이아웃 페이지는 전체 사이트 골격을 잡습니다.-->
+<template>
+  <div>
+    <about-container />
+    <div id="container" />
+  </div>
+</template>
+
+<script>
+import AboutContainer from '../About/AboutContainer.vue';
+
+export default {
+  name: 'EmptyLayout',
+  components: {
+    AboutContainer,
+  },
+};
+</script>
+
+<style scoped>
+@import url('@/assets/css/init.css');
+@import url('@/assets/css/root.css');
+@import url('@/assets/css/typography.css');
+
+#container {
+  position: absolute;
+  border-left: 1px solid var(--light-color-white);
+  border-right: 1px solid var(--light-color-white);
+  width: 430px;
+  height: fit-content;
+  min-height: 100%;
+  /* 메인 컨테이너를 가운데 정렬합니다. */
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
