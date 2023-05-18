6c8c95be3372fc87bb8ec144a33d7a1b0c0d37e7
bmincof <bminlvcoffee@gmail.com>
2023-05-18 13:32:47 +0900

    fix: LoginRequest 클래스 누락된 getter/setter 추가

--- server/pulsar/src/main/java/com/hammer/pulsar/dto/common/LoginRequest.java
+++ server/pulsar/src/main/java/com/hammer/pulsar/dto/common/LoginRequest.java
@@ -4,4 +4,34 @@ package com.hammer.pulsar.dto.common;
 public class LoginRequest {
     private String email;
     private String password;
+
+    // 기본 생성자
+    public LoginRequest() {}
+
+    // Getters, Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "LoginRequest{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+    
 }
