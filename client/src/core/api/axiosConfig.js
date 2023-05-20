3b01867a0fd8c99fca0a8d8a03f0c46df3cc584b
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-20 19:08:28 +0900

    fix: call 함수의 uri에 root uri가 반영되도록 수정

--- client/src/core/api/axiosConfig.js
+++ client/src/core/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //base url을 가져옵니다. 코드상으로 공개되지 않으며 .env파일에 있습니다
 //.env는 git에 업로드하지 않으며, git repository secret에서 이 부분을 관리합니다.
 // const root = process.env.VUE_APP_BASE_URL;
-const root = `http://localhost:8080/`;
+const root = `http://localhost:8080`;
 
 //AUTH가 필요하지 않은 요청입니다.
 //content-type 헤더를 설정하면 stringify하지 않아도 json으로 알아서 잘 갑니다.
@@ -32,7 +32,7 @@ AUTH_INSTANCE.defaults.withCredentials = true;
 
 /**
  * ### 실질적으로 API CALL을 통해 결과값을 리턴하는 함수입니다.
- * 파라미터로 `URI`, `method`, `data`, `authType`을 받습니다.
+ * 파라미터로 `uri`, `method`, `data`, `authType`을 받습니다.
  * authType 및 HTTP method에 따라 `axios` 인스턴스를 생성,
  * 요청을 보내 결과값을 받습니다. `method`에 아무것도 입력하지 않을 경우 `get`,
  * `authType`은 `normal`로 각각 기본값이 적용됩니다.
@@ -42,15 +42,16 @@ AUTH_INSTANCE.defaults.withCredentials = true;
  * @param {'normal'|'auth'} authType
  * @returns `Promise<Object>`
  */
-const call = async (URI, method = 'get', data, authType = 'normal') => {
+const call = async (uri, method = 'get', data, authType = 'normal') => {
   //bearer auth가 필요하면 authType에 문자열 'auth'를 입력하면 됩니다.
   const instance =
     authType === 'auth' ? AUTH_INSTANCE : NORMAL_INSTANCE;
-
+  const URI = root + uri;
   try {
     let result;
     if (method === 'get') result = await instance.get(URI);
-    if (method === 'post') result = await instance.post(URI, data);
+    if (method === 'post')
+      result = await instance.post(root + URI, data);
     if (method === 'put') result = await instance.put(URI, data);
     if (method === 'delete') result = await instance.delete(URI);
     //promise 객체
---
+++ client/src/module/landing/Landing.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="landing_wrapper">
+    <view-container api-call="apiCall">
+      <view-base title=""> </view-base>
+    </view-container>
+  </div>
+</template>
+
+<style scoped>
+@import url('../../assets/css/init.css');
+@import url('../../assets/css/root.css');
+@import url('../../assets/css/typography.css');
+</style>
+
+<script>
+import ViewBase from '../common/ViewBase.vue';
+import ViewContainer from '../common/ViewContainer.vue';
+
+export default {
+  name: '',
+  components: { ViewContainer, ViewBase },
+  data() {
+    return {};
+  },
+  methods: {
+    staticDataCall() {
+      return Promise.all([]).then((res) => res.data);
+    },
+  },
+};
+</script>
