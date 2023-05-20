b45a0e95be9f1d1ed02747773a159f488e7ec6c6
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-20 18:54:38 +0900

    feat: 모든 api 요청을 구현합니다.

--- client/src/core/api/community.js
+++ client/src/core/api/community.js
@@ -1,17 +1,80 @@
-//게시판 글 리스트 보기
+import call from './axiosConfig';
 
-//글 상세보기 - static datas
+/**
+ * ### 게시판의 글 리스트를 10개씩 조회합니다.
+ * @returns `Promise` with CODE(200, 500)
+ * */
+export const getArticleList = () => call('/community');
 
-//글 상세보기 - active data - 북마크 여부 확인
+/**
+ * ### `articleNo`번 글의 상세 정보를 리턴합니다.
+ * @param {number} articleNo
+ * @returns `Promise` with CODE(200, 500, 404)
+ */
+export const getArticleDetail = (articleNo) =>
+  call(`/community/${articleNo}/static`); //글 상세보기 - static datas
 
-//글 상세보기 - active data - 북마크하기, 북마크 취소하기
+/**
+ * ### `articleNo`번 글을 수정합니다.
+ * @param {number} articleNo
+ * @param {Object} data
+ * @returns `Promise` with CODE (200, 500, 401, 418)
+ */
+export const putArticle = (articleNo, data) =>
+  call(`/community/${articleNo}`, 'put', data, 'auth'); //글 수정하기
 
-//글 상세보기 - active data - 추천 수 가져오기
+/**
+ * ### `articleNo`번 글을 삭제합니다.
+ * @param {number} articleNo
+ * @returns `Promise` with CODE (200, 500, 401, 418)
+ */
+export const deleteArticle = (articleNo) =>
+  call(`/community/${articleNo}`, 'delete', [], 'auth'); //글 삭제하기
 
-//글 상세보기 - active data - 추천하기, 추천 취소하기
+/**
+ * ### `articleNo`번 글의 좋아요 여부 및 개수를 확인합니다.
+ * @param {number} articleNo
+ * @returns `Promise` with CODE (200, 500, 404)
+ */
+export const getArticleLike = (articleNo) =>
+  call(`/community/${articleNo}/active/like`); //
 
-//글 상세보기 - active data - 댓글 가져오기
+/**
+ * ### `articleNo`번 글에 좋아요가 되어있다면 좋아요를 취소, 그렇지 않다면 좋아요 처리합니다.
+ * @param {number} articleNo
+ * @returns `Promise` with CODE (200, 500, 404, 401)
+ */
+export const postArticleLike = (articleNo) => {
+  call(`/community/${articleNo}/active/like`, 'post', [], 'auth');
+};
 
-//글 상세보기 - active data - 댓글 작성
+/**
+ * ### `articleNo`번 글의 댓글을 모두 가져옵니다.
+ * @param {number} articleNo
+ * @returns `Promise` with CODE (200, 500, 404)
+ */
+export const getArticleComment = (articleNo) =>
+  call(`/community/${articleNo}/active/comment`, 'get', [], 'auth'); //글 상세보기 - active data - 댓글 가져오기
 
-//글 상세보기 - active data - 댓글 삭제
+/**
+ * ### `articleNo`번 글에 댓글을 작성합니다.
+ * @param {number} articleNo
+ * @param {Object} data
+ * @returns `Promise` with CODE (200, 500, 401, 418)
+ */
+export const postArticleComment = (articleNo, data) =>
+  call(`/community/${articleNo}/active/comment`, 'post', data, 'auth');
+
+/**
+ * ### `articleNo`번 글의 `commentNo`번 댓글을 삭제합니다.
+ * @param {number} articleNo
+ * @param {number} commentNo
+ * @returns `Promise` with CODE (200, 500, 401, 418)
+ */
+export const deleteArticleComment = (articleNo, commentNo) =>
+  call(
+    `/community/${articleNo}/active/comment/${commentNo}`,
+    'delete',
+    [],
+    'auth'
+  );
--- client/src/core/api/routine.js
+++ client/src/core/api/routine.js
@@ -1,15 +1,41 @@
-//내 루틴 리스트 가져오기
+import call from './axiosConfig';
 
-//루틴 상세보기
+/**
+ * ### `memberNo`번 유저의 모든 루틴을 가져옵니다.
+ * @param {number} memberNo
+ * @returns Promise with CODE (200, 500, 401)
+ */
+export const getRoutines = (memberNo) =>
+  call(`/routines/${memberNo}`, 'get', [], 'auth');
 
-//루틴 작성
+/**
+ * ### `routineNo`번 루틴의 상세 정보를 가져옵니다.
+ * @param {number} routineNo
+ * @returns Promise with CODE (200, 500, 401)
+ */
+export const getRoutineDetail = (routineNo) =>
+  call(`/routines/routine/${routineNo}`, 'get', [], 'auth');
 
-//루틴 수정
+/**
+ * ### 루틴을 작성합니다.
+ * @param {Object} data
+ * @returns `Promise` with CODE (200, 500, 401, 418)
+ */
+export const postRoutine = (data) =>
+  call(`/routines/routine`, 'post', data, 'auth');
 
-//루틴 삭제
+/**
+ * ### `routineNo`번 루틴을 수정합니다. 또는, 루틴에 운동 리스트를 추가합니다.
+ * @param {Object} data
+ * @returns `Promise` with CODE (200, 500, 401, 418)
+ */
+export const putRoutine = (routineNo, data) =>
+  call(`/routines/routine/${routineNo}`, 'put', data, 'auth');
 
-//exercise 작성
-
-//exercise 수정
-
-//exercise 삭제
+/**
+ * ### `routineNo`번 루틴을 삭제합니다.
+ * @param {number} routineNo
+ * @returns `Promise` with CODE (200, 500, 401, 418)
+ */
+export const deleteRoutine = (routineNo) =>
+  call(`/routines/routine/${routineNo}`, 'delete', [], 'auth');
