639bee2d87015053db318543dbd6c0b86c54f682
bmincof <bminlvcoffee@gmail.com>
2023-05-22 10:35:12 +0900

    feat: 로그인 상태를 확인하기 위한 인터셉터 구현

---
+++ server/pulsar/src/main/java/com/hammer/pulsar/interceptor/AuthCheckInterceptor.java
@@ -0,0 +1,21 @@
+package com.hammer.pulsar.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthCheckInterceptor implements HandlerInterceptor {
+    private final Logger logger = LoggerFactory.getLogger(AuthCheckInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("userNo = {}", request.getSession().getAttribute("loginInfo"));
+
+        return true;
+    }
+}
