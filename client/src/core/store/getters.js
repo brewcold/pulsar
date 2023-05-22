3f69ff0271cb0a0acb1837198a979629db359c6c
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-22 17:15:38 +0900

    feat: vuex에서 sessionId를 관리하도록 수정

--- client/src/core/store/getters.js
+++ client/src/core/store/getters.js
@@ -1,9 +1,12 @@
 //state의 값을 가져옵니다.
 export default {
-  getLogined() {
-    return state.authenticated;
+  getLogined({ authenticated }) {
+    return authenticated;
   },
   getLoginUser(state) {
     return state;
+  },
+  getSessionId({ sessionId }) {
+    return sessionId;
   },
 };
