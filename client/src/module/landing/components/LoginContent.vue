0052695e2f43ea29cd9115df5e03bd8bbdfb8adf
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-22 15:15:53 +0900

    design: 버튼 텍스트 및 margin값을 수정

--- client/src/module/landing/components/LoginContent.vue
+++ client/src/module/landing/components/LoginContent.vue
@@ -3,31 +3,33 @@
     <!-- 상위 컴포넌트에서 v-model은 하위 컴포넌트에서 emit해 바인딩할 수 있습니다.
          v-model이 하위 컴포넌트에서의 emit + 그 하위 컴포넌트로 pass prop인 것을 이용한 것입니다. -->
     <h2>PULSAR에 오신 것을 환영합니다!</h2>
-    <p class="login_content_caption">
+    <p id="login_content_caption">
       서비스를 이용하려면 로그인해야 합니다.
     </p>
-    <text-input
-      :input-name="'Email'"
-      :placeholder="'Email'"
-      :type="'email'"
-      :margin="'1rem'"
-      v-model="email"
-    />
-    <text-input
-      :input-name="'Password'"
-      :placeholder="'Password'"
-      :type="'password'"
-      :margin="'1rem'"
-      :caption="'영문 소문자, 숫자, 특수문자 포함 8자리 이상'"
-      v-model="password"
-    />
-    <square-button
-      :theme="'highlight'"
-      :value="'LOGIN'"
-      @handle-click="handleClick"
-    />
+    <form @keyup.enter="handleBtn">
+      <text-input
+        :input-name="'Email'"
+        :placeholder="'Email'"
+        :inputType="'email'"
+        :margin="'1rem'"
+        v-model="email"
+      />
+      <text-input
+        :input-name="'Password'"
+        :placeholder="'Password'"
+        :inputType="'password'"
+        :margin="'3rem'"
+        v-model="password"
+      />
+      <square-button
+        :theme="'highlight'"
+        :value="'LOGIN'"
+        @handle-click="handleBtn"
+        id="login_content_loginBtn"
+      />
+    </form>
     <router-link to="/signup">
-      <square-button :theme="'white'" :value="'SIGN UP'" />
+      <square-button :theme="'white'" :value="'아직 회원이 아니에요'" />
     </router-link>
   </div>
 </template>
@@ -40,8 +42,11 @@
 #login_content {
   padding: 1.5rem;
 }
-.login_content_caption {
+#login_content_caption {
   margin: 1rem 0 2rem 0;
+}
+#login_content_loginBtn {
+  margin-bottom: 1rem;
 }
 </style>
 
@@ -60,7 +65,7 @@ export default {
     };
   },
   methods: {
-    handleClick() {
+    handleBtn() {
       //로그인을 시도하기 위해 위쪽으로 로그인 이벤트와 값을 보내고,
       //양방향 바인딩되어 있는 데이터 쪽의 값을 초기화해 인풋을 빈 값으로 만듭니다.
       this.$emit('login', this.email, this.password);
