3dfdf0849f3f4109a886b62a92bf5c235e136257
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-22 15:12:07 +0900

    feat: input type=password이고 캡션이 있으면, 비밀번호 검사 결과를 실시간으로 표시합니다

--- client/src/module/common/TextInput.vue
+++ client/src/module/common/TextInput.vue
@@ -9,7 +9,7 @@
         :type="inputType"
         :placeholder="placeholder"
         :value="value"
-        @change="$emit('input', $event.target.value)"
+        @keyup="$emit('input', $event.target.value)"
       />
       <p class="caption">{{ caption }}</p>
     </div>
@@ -31,10 +31,22 @@ export default {
     margin: String,
   },
   computed: {
+    isValid() {
+      const regex =
+        /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[a-z\d@$!%*#?&]{8,}$/i;
+      return regex.test(this.value);
+    },
     styles() {
       return {
         '--margin': this.margin,
         '--display': this.caption ? 'block' : 'none',
+        '--caption-color':
+          //중첩 삼항연산자입니다. inputType이 password이고 비밀번호가 valid하다면...
+          this.inputType === 'password'
+            ? this.isValid
+              ? '#09DF78'
+              : '#DF093D'
+            : '#666666',
       };
     },
   },
@@ -63,7 +75,7 @@ input::placeholder {
   color: var(--light-color-grey);
 }
 .caption {
-  color: var(--light-color-darkgrey);
+  color: var(--caption-color);
   text-align: right;
   font-size: 0.85rem;
   padding: 0.5rem 0 0.5rem 0;
