fc8bf721047544ada2522d79a1e10ba660f1d4b5
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-23 11:53:31 +0900

    fix: 의존성 조정 및 삭제로 svg 이미지가 제대로 보이도록 조정

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+// module.exports = {
+//   chainWebpack: (config) => {
+//     const svgRule = config.module.rule('svg');
+
+//     svgRule.uses.clear();
+
+//     svgRule
+//       .use('babel-loader')
+//       .loader('babel-loader')
+//       .end()
+//       .use('vue-svg-loader')
+//       .loader('vue-svg-loader');
+//   },
+// };
