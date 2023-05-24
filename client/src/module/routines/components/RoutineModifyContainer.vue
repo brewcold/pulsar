92d66b4fa4d819e0e5769e313aed439613091bf6
s-y-yu <seung.yoon.yu@gmail.com>
2023-05-24 18:59:04 +0900

    feat: 루틴 리스트, 상세 화면 세부 구현

--- client/src/module/routines/components/RoutineModifyContainer.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>wow</h1>
-</template>
--- client/src/module/routines/router/routines.js
+++ client/src/module/routines/router/routines.js
@@ -2,8 +2,8 @@ import RoutinesView from '../RoutinesView.vue';
 import RoutineDetailContainer from '../components/RoutineDetailContainer.vue';
 import RoutineDetailWrapper from '../components/RoutineDetailWrapper.vue';
 import RoutineListContainer from '../components/RoutineListContainer.vue';
-import RoutineModifyContainer from '../components/RoutineModifyContainer.vue';
-import RoutineWriteContainer from '../components/RoutineWriteContainer.vue';
+// import RoutineModifyContainer from '../components/RoutineModifyContainer.vue';
+// import RoutineWriteContainer from '../components/RoutineWriteContainer.vue';
 
 export default [
   {
@@ -14,21 +14,21 @@ export default [
         path: '',
         component: RoutineListContainer,
       },
+      // {
+      //   path: 'write', //routines/write (새 루틴 작성)
+      //   component: RoutineWriteContainer,
+      // },
       {
         path: ':routineNo', //routines/:routineNo (루틴 상세보기)
         component: RoutineDetailWrapper,
         children: [
           { path: '', component: RoutineDetailContainer },
-          {
-            path: 'modify', //routines/:routineNo/modify (루틴 수정)
-            component: RoutineModifyContainer,
-          },
+          // {
+          //   path: 'modify', //routines/:routineNo/modify (루틴 수정)
+          //   component: RoutineModifyContainer,
+          // },
           //세부 운동 종목 추가는 모달로 설정
         ],
-      },
-      {
-        path: 'write', //routines/write (새 루틴 작성)
-        component: RoutineWriteContainer,
       },
     ],
   },
